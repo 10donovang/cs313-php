ab3cd4022d56f5e63bd324de37a5ca78173a0c6e
Donovan Gregory <Donovan Gregory>
2017-05-13 16:02:47 -0600

    Added Store pages

--- web/confirmation.php
+++ web/confirmation.php
@@ -7,17 +7,17 @@ session_start();
  <body>
  <?php
  	echo "Your shopping list includes: <br>";
-    echo "The Best Magic Tricks of All Times by Donald McGregor: " . $_SESSIONS["n1"];
+    echo "The Best Magic Tricks of All Times by Donald McGregor: " . $_SESSIONS["n1"] . "<br>";
 
-	echo "Math Tricks for All Times by Donald McGregor: " . $_SESSIONS["n2"];
+	echo "Math Tricks for All Times by Donald McGregor: " . $_SESSIONS["n2"] . "<br>";
 
-	echo "The Deft Book of Magic by Donald McGregor: " . $_SESSIONS["n3"];
+	echo "The Deft Book of Magic by Donald McGregor: " . $_SESSIONS["n3"] . "<br>";
 
 	echo "Magic Book of Math Tricks by Donald McGregory: " . $_SESSIONS["n4"];
 
-	echo "<br> Name: " . $_POST["name"];
-	echo "E-mail: " . $_POST["email"];
-	echo "Phone: " . $_POST["phone"];
+	echo "<br> Name: " . $_POST["name"] . "<br>";
+	echo "E-mail: " . $_POST["email"] . "<br>";
+	echo "Phone: " . $_POST["phone"] . "<br>";
 	echo "Address: " . $_POST["address"] . " " . $_POST["city"] . ", " . $_POST["state"] . " " . $_POST["zip"];
 	?>
 
