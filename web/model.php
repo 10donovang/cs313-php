6836444ddaf926cb09171799ce20f45797b9b50f
Donovan Gregory <Donovan Gregory>
2017-05-16 10:30:35 -0600

    sql

--- web/model.php
+++ web/model.php
@@ -1,8 +1,19 @@
 <?php
-function getScripture(){
+function getScriptures(){
     global $db;
     $query = 'SELECT * FROM scriptures';
     $statement = $db->prepare($query);
+    $statement->execute();
+    $scripture = $statement->fetchAll();
+    $statement->closeCursor();
+    return $scripture;
+}
+
+function getScripture($id){
+    global $db;
+    $query = 'SELECT * FROM scriptures WHERE scripture_id = :id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":id", $id);
     $statement->execute();
     $scripture = $statement->fetchAll();
     $statement->closeCursor();
--- web/scriptures.php
+++ web/scriptures.php
@@ -8,9 +8,17 @@
         require ('connect.php');
         echo "<h1><strong>Scripture Resources</strong></h1>";
         if(!isset($_GET['view'])){
-        $scriptures = getScripture();
+        $scriptures = getScriptures();
         foreach($scriptures as $scripture){
         	echo "<p><strong>" . $scripture['book'] . " " . $scripture['chapter'] . ":" . $scripture['verse'] . '</strong> - <a href="scriptures.php?id=' . $scripture['scripture_id'] . '"> View Scripture</a></p>';
+        }
+    }
+
+    if(isset($_GET['id'])){
+    	$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+    	$scriptures = getScripture($id);
+        foreach($scriptures as $scripture){
+        	echo "<p><strong>" . $scripture['book'] . " " . $scripture['chapter'] . ":" . $scripture['verse'] . '</strong> - "' . $scripture['content'] . '"</p>';
         }
     }
 
