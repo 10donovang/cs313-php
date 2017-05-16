0461494d650eeceb0b8ff2c8d55cc0fd2328c1c8
Donovan Gregory <Donovan Gregory>
2017-05-16 10:06:11 -0600

    sql

--- web/connect.php
+++ web/connect.php
@@ -1,8 +1,9 @@
 <?php
 
-$dbuser = 'postgres';
-$host = '127.0.0.1';
-$dbname = 'calm-depths-42678';
-$db = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser);
+$dbuser = 'dzuyuopgjyjdiq';
+$host = 'ec2-23-23-227-188.compute-1.amazonaws.com';
+$dbname = 'd97gn4ualml6qq';
+$dbpass = '99556258ef88831cdabae4b53ba822c7c3f87ed453fffd81252d8c71d86a1bee';
+$db = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass);
 
 ?>
