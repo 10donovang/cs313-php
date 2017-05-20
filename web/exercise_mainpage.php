818a74195031746a3a7f4cfeb7c336a0c47d131f
Donovan Gregory <Donovan Gregory>
2017-05-20 16:49:25 -0600

    Exercise Website

---
+++ web/exercise_mainpage.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+        <body>
+        <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+		
+        require ('exercise_model.php');
+        require ('connect.php');
+        echo "<h1><strong>Workout Selection</strong></h1>";
+        echo'
+        <form action="exercise.php">
+    <select name="type">
+    <option value="Cross Fit">Cross Fit</option>
+    </select>
+    <select name="time">
+    <option value="Long">Long</option>
+    <option value="Med">Medium</option>
+    <option value="Short">Short</option>
+    </select>
+    <br><br>
+    <input type="submit">
+    </form>';
+        ?>
+        </body>
+        </html>
+         
