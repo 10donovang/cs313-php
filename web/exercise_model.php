1b1c35e10b4767a38bab3f38ef33da437b892b16
Donovan Gregory <Donovan Gregory>
2017-05-27 14:10:07 -0600

    test

--- web/exercise_model.php
+++ web/exercise_model.php
@@ -1,7 +1,7 @@
 <?php
 function getWorkoutChoice($time, $type){
     global $db;
-    $query = 'SELECT * FROM workouts WHERE duration = 'Long'';
+    $query = 'SELECT * FROM workouts WHERE duration = "Long"';
     $statement = $db->prepare($query);
     $statement->execute();
     $workouts = $statement->fetchAll();
