026ea23d1fb8d3ea8805b188d22b1116e32199de
Donovan Gregory <Donovan Gregory>
2017-05-27 13:32:11 -0600

    test

--- web/exercise_mainpage.php
+++ web/exercise_mainpage.php
@@ -9,7 +9,7 @@ ini_set("display_errors", 1);
         require ('connect.php');
         echo "<h1><strong>Workout Selection</strong></h1>";
         echo'
-        <form action="exercise_mainpage.php">
+        <form action="exercise_mainpage.php" method="GET">
     <select name="type">
     <option value="Cross Fit">Cross Fit</option>
     </select>
