602b44dd059a692e16c205abef0df35cbe223055
Donovan Gregory <Donovan Gregory>
2017-05-27 15:38:45 -0600

    test

--- web/exercise_choices.php
+++ web/exercise_choices.php
@@ -19,7 +19,10 @@ ini_set("display_errors", 1);
     if(isset($_GET['id'])){
 
 $id = "'" . $_GET['id'] . "'";
+
 $routines = getWorkout($id);
+foreach($routines as $routine){
+	echo "<p>" .$routine['workout_1'] . "</p>";
 }
 }
           ?>
