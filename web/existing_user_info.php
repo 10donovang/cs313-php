e823ae5f0a3c3e517ab71d1c89626ca2ec40b867
Donovan Gregory <Donovan Gregory>
2017-05-28 02:50:38 -0600

    test

--- web/existing_user_info.php
+++ web/existing_user_info.php
@@ -20,7 +20,7 @@ $id = $user['user_id'];}
 
 echo '<form action="" method="get">
 Workout Name: <input type="text" name="wname"><br>
-Notes: <textarea name="notes" cols="40" rows = "8"></textarea><br>
+
 <input type="submit">
 </form> '
 
