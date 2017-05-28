f7e34c56b39d6b3def05c33c62cf00f417ba78c0
Donovan Gregory <Donovan Gregory>
2017-05-28 01:11:51 -0600

    test

---
+++ web/existing_user.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+        <body>
+        <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+		
+        require ('exercise_model.php');
+        require ('connect.php');
+
+        ?>
+        </body>
+        </html>
---
+++ web/new_user.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+        <body>
+        <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+		
+        require ('exercise_model.php');
+        require ('connect.php');
+        echo '<form action="existing_user.php" method="post">
+First Name: <input type="text" name="fname"><br>
+Middle Name: <input type="text" name="mname"><br>
+Last Name: <input type="text" name="lname"><br>
+User Name: <input type="text" name="uname"><br>
+Password: <input type="text" name="pass"><br>
+<input type="submit">
+</form>
+
+        '
+
+
+
+        ?>
+        </body>
+        </html>
